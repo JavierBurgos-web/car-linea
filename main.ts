492b4594b028df7b4ba155fa070d1631c8ffc048
JavierBurgos-web <84932679+JavierBurgos-web@users.noreply.github.com>
2022-10-20 19:40:28 -0500

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,27 @@
+music.startMelody(music.builtInMelody(Melodies.JumpUp), MelodyOptions.Once)
+basic.showString("¡Hello!")
+basic.showIcon(IconNames.Happy)
+basic.pause(2000)
+basic.clearScreen()
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P1) == 0 && pins.digitalReadPin(DigitalPin.P16) == 0) {
-        basic.showIcon(IconNames.Happy)
-        pins.digitalWritePin(DigitalPin.P0, 1)
-        pins.digitalWritePin(DigitalPin.P2, 1)
+        pins.analogWritePin(AnalogPin.P0, 512)
+        pins.analogWritePin(AnalogPin.P2, 512)
+        basic.pause(100)
     }
     if (pins.digitalReadPin(DigitalPin.P1) == 0 && pins.digitalReadPin(DigitalPin.P16) == 1) {
-        pins.digitalWritePin(DigitalPin.P0, 1)
-        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.analogWritePin(AnalogPin.P0, 512)
+        pins.analogWritePin(AnalogPin.P2, 0)
+        basic.pause(100)
     }
     if (pins.digitalReadPin(DigitalPin.P1) == 1 && pins.digitalReadPin(DigitalPin.P16) == 0) {
-        pins.digitalWritePin(DigitalPin.P0, 0)
-        pins.digitalWritePin(DigitalPin.P2, 1)
+        pins.analogWritePin(AnalogPin.P0, 0)
+        pins.analogWritePin(AnalogPin.P2, 512)
+        basic.pause(100)
     }
     if (pins.digitalReadPin(DigitalPin.P1) == 1 && pins.digitalReadPin(DigitalPin.P16) == 1) {
-        basic.showIcon(IconNames.Surprised)
-        pins.digitalWritePin(DigitalPin.P0, 0)
-        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.analogWritePin(AnalogPin.P0, 0)
+        pins.analogWritePin(AnalogPin.P2, 0)
+        basic.pause(100)
     }
 })

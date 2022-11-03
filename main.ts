3de87375ccba41931e5fe0ac50f027413e7c6d33
JavierBurgos-web <84932679+JavierBurgos-web@users.noreply.github.com>
2022-11-03 13:48:52 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,21 @@
-music.startMelody(music.builtInMelody(Melodies.JumpUp), MelodyOptions.Once)
-basic.showString("¡Hello!")
-basic.showIcon(IconNames.Happy)
-basic.pause(2000)
+pins.analogWritePin(AnalogPin.P0, 0)
+pins.analogWritePin(AnalogPin.P2, 0)
+basic.showLeds(`
+    # . # . #
+    # . # . #
+    # . # # #
+    . . . . #
+    # . . . #
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    . # # # .
+    # . . . #
+    . . . . .
+    . # . # .
+    . . . . .
+    `)
+basic.pause(500)
 basic.clearScreen()
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P1) == 0 && pins.digitalReadPin(DigitalPin.P16) == 0) {
